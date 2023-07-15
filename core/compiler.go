11df5b7ac1344b84002bf1c00dee588f288eafc2
bjarneo <bjarne.oeverli@gmail.com>
2023-07-15 17:12:30 +0200

    feat: define your own key value pairs by using -kv=name,value

--- core/compiler.go
+++ core/compiler.go
@@ -1,23 +1,11 @@
 package core
 
-func Compiler(input string, kind string) (string, map[string]interface{}) {
-	// predefine our output string
-	output := ""
-
+func Compiler(input string, kind string) map[string]interface{} {
 	// Create tokens
 	tokens := Tokenizer(input)
 	// Transform the token into an "AST" (not really an AST though)
 	AST := Transformer(tokens)
 
-	if kind == "json" {
-		// Generate JSON from the "AST"
-		output = JSONGenerator(AST)
-	}
-
-	if kind == "yaml" {
-		output = YAMLGenerator(AST)
-	}
-
 	// return the JSON string
-	return output, AST
+	return AST
 }
--- core/jsonGenerator.go
+++ core/jsonGenerator.go
@@ -3,10 +3,11 @@ package core
 import (
 	"encoding/json"
 	"log"
+	"strings"
 )
 
-func JSONGenerator(ast map[string]interface{}) string {
-	json, err := json.Marshal(ast)
+func JSONGenerator(AST any) string {
+	json, err := json.Marshal(AST)
 
 	if err != nil {
 		log.Fatal(err)
@@ -14,4 +15,29 @@ func JSONGenerator(ast map[string]interface{}) string {
 
 	// return the json as a string
 	return string(json)
+}
+
+type M map[string]interface{}
+
+func TransformKV(AST map[string]interface{}, kv string) []M {
+	var output []M
+
+	kvData := strings.Split(kv, ",")
+
+	for key, value := range AST {
+		output = append(output, M{kvData[0]: key, kvData[1]: value})
+	}
+
+	// return the json as a string
+	return output
+}
+
+func PrettyPrint(AST any) string {
+	prettyPrint, err := json.MarshalIndent(AST, "", "  ")
+
+	if err != nil {
+		log.Fatalf(" [!] Something happened while creating the pretty print JSON \n %s", err)
+	}
+
+	return string(prettyPrint)
 }
